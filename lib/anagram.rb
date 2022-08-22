169e4354342fac97ef85e5f3a09306b2d97274df
mbolonzi-design <mbolonzi.design@gmail.com>
2022-08-22 08:22:06 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(word)
+        @name = word
+    end
+
+    def match(arry)
+        arry.select { |x| x.split("").sort == @name.split("").sort}
+    end
+end
